20d0e9b082d2e8ea69e962aa3277d0446e655622
Kevin Minutti <minutti70@gmail.com>
2020-11-21 12:18:55 -0500

    'tested associations'

--- db/models/student.js
+++ db/models/student.js
@@ -1,7 +1,7 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Student = db.define('student', {
+const Student = db.define("student", {
   firstName: {
     type: Sequelize.STRING,
     allowNull: false,
