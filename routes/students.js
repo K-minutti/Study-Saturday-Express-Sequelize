a0d9cdd1e581a3f645851e94da7b992f45ad8e6d
Kevin Minutti <minutti70@gmail.com>
2020-11-21 14:52:12 -0500

    'Did something'

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Student = require("../db/models/student");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const students = await Student.findAll();
+    res.send(students);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
+    if (student) {
+      res.send(student);
+    } else {
+      res.status(404).send(`You f'd up bro`);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/:id", (req, res, next) => {});
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,6 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const test = require("../db/models/test");
 
+// router.get("/tests", (req, res, next) => {});
 
 module.exports = router;
