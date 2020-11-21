fe7e18ce1b601e827b444e1b8d08f7463954be47
Kevin Minutti <minutti70@gmail.com>
2020-11-21 10:39:09 -0500

    'passed all specs'

--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,29 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Student = db.define();
+const Student = db.define("student", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+Student.beforeSave(function (user) {
+  user.firstName =
+    user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+  user.lastName =
+    user.lastName.charAt(0).toUpperCase() + user.lastName.slice(1);
+});
 
 module.exports = Student;
